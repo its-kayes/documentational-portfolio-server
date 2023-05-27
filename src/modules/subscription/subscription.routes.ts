6d54ab26a556205ca98b9262390acb44a721e332
Kayes <kayes.ek8@gmail.com>
2023-05-27 19:27:52 +0600

    implement get Subscribers

--- src/modules/subscription/subscription.routes.ts
+++ src/modules/subscription/subscription.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { addSubscriber } from "./subscription.controller";
+import { addSubscriber, getSubscribers } from "./subscription.controller";
 const router: Router = Router();
 
 router.post("/subscribe", addSubscriber);
+router.get("/subscribers", getSubscribers);
 
 export { router as subscriptionRoutes };
